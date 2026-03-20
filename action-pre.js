31cd1e3175a0138d0bc0d83e71e8eba80683882b
Adrian Kunz <a.kunz@uni-kassel.de>
2026-03-20 20:57:26 +0100

    chore: Try -g

--- action-pre.js
+++ action-pre.js
@@ -1,7 +1,7 @@
 const cwd = process.cwd();
-console.log(cwd);
 require('child_process').spawnSync('npm', [
     'install',
+    '-g',
     'vscode-json-languageservice',
     'jsonc-parser'
 ], {stdio: [0, 1, 2], cwd});
